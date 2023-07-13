dcd20f7443acc603c49deadd20a4f73ff91e5c12
HusseinMohamed <husseinhtm99@gmail.com>
2023-07-13 23:31:55 +0300

    onBoardingScreen ✔ prefScreen ✔ assets ✔ themeMode ✔ splashScreen ✔ components ✔ Date: 11/7/2023 Add Firebase userModel registerCubit registerScreen ✔ localization Laboar Cubit

--- lib/core/components/buttons.dart
+++ lib/core/components/buttons.dart
@@ -11,30 +11,39 @@ Widget defaultMaterialButton({
   double? radius,
   bool isUpperCase = false,
   Color? color,
-  Function? onTap,
 }) =>
-    Container(
-      alignment: Alignment.center,
-      width: width?.w ?? 388.w,
-      height: height?.h ?? 45.h,
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(
-          radius?.r ?? 16.r,
+    GestureDetector(
+      onTap: function,
+      child: Container(
+        alignment: Alignment.center,
+        width: width?.w ?? 388.w,
+        height: height?.h ?? 45.h,
+        decoration: ShapeDecoration(
+          shape: RoundedRectangleBorder(
+            side: const BorderSide(
+              width: 0.50,
+              strokeAlign: BorderSide.strokeAlignCenter,
+              color: Color(0xFFDFDFDF),
+            ),
+            borderRadius: BorderRadius.circular(
+              radius?.r ?? 8.r,
+            ),
+          ),
+          color: color ?? AppColorsLight.lightPrimaryColor,
         ),
-        color: color ?? AppColorsLight.lightPrimaryColor,
-      ),
-      child: MaterialButton(
-        clipBehavior: Clip.antiAliasWithSaveLayer,
-        height: 22.h,
-        minWidth: 59.w,
-        onPressed: function,
-        child: Text(
-          textAlign: TextAlign.center,
-          isUpperCase ? text.toUpperCase() : text,
-          style: GoogleFonts.quicksand(
-            fontSize: 18.sp,
-            color: AppColorsLight.textWhiteColor,
-            fontWeight: FontWeight.w700,
+        child: MaterialButton(
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          height: 45.h,
+          minWidth: 388.w,
+          onPressed: function,
+          child: Text(
+            textAlign: TextAlign.center,
+            isUpperCase ? text.toUpperCase() : text,
+            style: GoogleFonts.quicksand(
+              fontSize: 18.sp,
+              color: AppColorsLight.textWhiteColor,
+              fontWeight: FontWeight.w700,
+            ),
           ),
         ),
       ),
